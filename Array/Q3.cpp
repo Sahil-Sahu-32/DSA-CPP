3df11219e8c4cf1378594e06bbcb1da1d6aec5cf
sahil02-code <sahu02sahil@gmail.com>
2026-07-30 23:28:54 +0530

    Array Q3

---
+++ Array/Q3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int firstMissingPositive(vector<int>& nums) {
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++) {
+
+        while (nums[i] >= 1 &&
+               nums[i] <= n &&
+               nums[i] != nums[nums[i] - 1]) {
+
+            swap(nums[i], nums[nums[i] - 1]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] != i + 1)
+            return i + 1;
+    }
+
+    return n + 1;
+}
+
+int main() {
+
+    vector<int> nums = {3, 4, -1, 1};
+
+    cout << firstMissingPositive(nums);
+
+    return 0;
+}
