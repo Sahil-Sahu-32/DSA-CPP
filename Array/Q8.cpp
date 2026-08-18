84d6684f075319186781dd86e0fbb2d908dfc2d0
Sahil-Sahu-32 <sahu02sahil@gmail.com>
2026-08-18 23:05:12 +0530

    Array Q8 more

--- Array/Q8.cpp
+++ Array/Q8.cpp
@@ -21,4 +21,32 @@ int longestConsecutive(int arr[], int n)
         int num = arr[i];
 
         // Check whether num is the beginning
-  
+        if (st.find(num - 1) == st.end())
+        {
+            int current = num;
+            int length = 1;
+
+            // Find consecutive numbers
+            while (st.find(current + 1) != st.end())
+            {
+                current++;
+                length++;
+            }
+
+            longest = max(longest, length);
+        }
+    }
+
+    return longest;
+}
+
+int main()
+{
+    int arr[] = {100, 4, 200, 1, 3, 2};
+    int n = 6;
+
+    cout << "Longest consecutive sequence = "
+         << longestConsecutive(arr, n);
+
+    return 0;
+}
