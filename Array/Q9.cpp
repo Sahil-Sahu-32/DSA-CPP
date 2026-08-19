37eb7d6475a8506bef876e40063dd76a6b3eb0d3
Sahil-Sahu-32 <sahu02sahil@gmail.com>
2026-08-19 22:40:41 +0530

    Array Q9

---
+++ Array/Q9.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_set>
+#include <algorithm>
+using namespace std;
+
+int longestConsecutive(int arr[], int n)
+{
+    unordered_set<int> st;
+
+    // Store all elements
+    for (int i = 0; i < n; i++)
+    {
+        st.insert(arr[i]);
+    }
+
+    int longest = 0;
+
+    // Check every element
+    for (int i = 0; i < n; i++)
+    {
+        int num = arr[i];
+
+        // Check whether num is the beginning
+        if (st.find(num - 1) == st.end())
+        {
+            int current = num;
+            int length = 1;
