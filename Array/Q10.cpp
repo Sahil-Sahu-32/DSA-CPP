105210cd5a7639861214e0a09ca75cdcb5eec979
Sahil-Sahu-32 <sahu02sahil@gmail.com>
2026-08-21 23:10:52 +0530

    Array Q10

---
+++ Array/Q10.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countEqual01(vector<int>& arr) {
+
+    unordered_map<int, int> freq;
+
+    // Empty prefix
+    freq[0] = 1;
+
+    int sum = 0;
+    int answer = 0;
+
+    for (int x : arr) {
+
+        // Convert:
+        // 0 -> -1
+        // 1 -> +1
+
+        if (x == 0) {
+            sum--;
+        }
+        else {
+            sum++;
+        }
+
+        // Same prefix sum means
+        // equal number of 0s and 1s
+        if (freq.find(sum) != freq.end()) {
