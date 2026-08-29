e2edb092eed22341a313f339161e807da7146e8f
Sahil-Sahu-32 <sahu02sahil@gmail.com>
2026-08-29 17:29:45 +0530

    Patterns Q2,3,4

--- practice.cpp
+++ practice.cpp
@@ -4,15 +4,69 @@ using namespace std;
 int main()
 {
 
+// Q1
+
+// int n;
+// cout<< "Enter number of lines";
+// cin>> n;
+
+// for(int i= 1; i<=n; i++){
+
+//     for (int j = 0; j < n; j++)
+//     {
+//         cout<< i <<" " ;
+//     }
+//     cout<< "\n";
+// }
+
+
+
+// Q2
+
+// int n;
+// cout<< "Enter number of lines";
+// cin>> n;
+
+// for(int i= 1; i<=n; i++){
+
+//     for (int j = 0; j < i; j++)
+//     {
+//         cout<< "*" <<"" ;
+//     }
+//     cout<< "\n";
+// }
+
+
+
+
+// Q3
+
+// int n;
+// cout<< "Enter number of lines";
+// cin>> n;
+
+// for(int i= 0; i<n; i++){
+
+//     for (int j = 0; j < n-i; j++)
+//     {
+//         cout<< "*" <<" " ;
+//     }
+//     cout<< "\n";
+// }
+
+
+
+//  Q4
+
 int n;
 cout<< "Enter number of lines";
 cin>> n;
 
 for(int i= 1; i<=n; i++){
 
-    for (int j = 0; j < n; j++)
+    for (int j = 1; j <= i; j++)
     {
-        cout<< i <<" " ;
+        cout<< j <<" " ;
     }
     cout<< "\n";
 }
