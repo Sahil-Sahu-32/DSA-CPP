19dd368739d243e480f052100d8f456ed52a4813
Sahil-Sahu-32 <sahu02sahil@gmail.com>
2026-08-29 17:32:02 +0530

    Patterns Q5

--- practice.cpp
+++ practice.cpp
@@ -58,15 +58,33 @@ int main()
 
 //  Q4
 
+// int n;
+// cout<< "Enter number of lines";
+// cin>> n;
+
+// for(int i= 1; i<=n; i++){
+
+//     for (int j = 1; j <= i; j++)
+//     {
+//         cout<< j <<" " ;
+//     }
+//     cout<< "\n";
+// }
+
+
+
+// Q5
+
 int n;
 cout<< "Enter number of lines";
 cin>> n;
-
+char character = 'A';
 for(int i= 1; i<=n; i++){
 
-    for (int j = 1; j <= i; j++)
+    for (int j = 0; j < i; j++)
     {
-        cout<< j <<" " ;
+        cout<< character <<" " ;
+        character++;
     }
     cout<< "\n";
 }
