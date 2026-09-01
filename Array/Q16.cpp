392768e9ef8bfdb3e8d3f276d5d5d89bc4eafac2
Sahil-Sahu-32 <sahu02sahil@gmail.com>
2026-09-01 21:31:53 +0530

    Array Q6

---
+++ Array/Q16.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int longestZeroSumSubarray(vector<int>& arr) {
+
+    unordered_map<long long, int> mp;
+
+    long long prefixSum = 0;
+    int maxLength = 0;
+
+    for (int i = 0; i < arr.size(); i++) {
+
+        prefixSum += arr[i];
+
+        // Case 1: subarray starts from index 0
+        if (prefixSum == 0) {
+            maxLength = i + 1;
+        }
+
+        // Case 2: prefix sum has appeared before
+        if (mp.find(prefixSum) != mp.end()) {
+
+            int length = i - mp[prefixSum];
+
+            maxLength = max(maxLength, length);
+        }
+        else {
+            // Store only first occurrence
+            mp[prefixSum] = i;
+        }
+    }
+
+    return maxLength;
+}
+
