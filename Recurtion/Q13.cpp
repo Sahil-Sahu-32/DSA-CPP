d8be46315b84ff6b2f3e81438c647ab773e7b951
Sahil-Sahu-32 <sahu02sahil@gmail.com>
2026-09-11 23:53:41 +0530

    Recuriton Q13

---
+++ Recurtion/Q13.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void findSubsequences(int arr[], int n, int index,
+                      int target, vector<int>& current) {
+
+    // Base case
+    if (index == n) {
+
+        if (target == 0) {
+            for (int x : current) {
+                cout << x << " ";
+            }
+            cout << endl;
+        }
+
+        return;
+    }
+
+    // Include current element
+    current.push_back(arr[index]);
+
+    findSubsequences(arr, n, index + 1,
+                      target - arr[index], current);
+
+    current.pop_back();
+
+    // Exclude current element
+    findSubsequences(arr, n, index + 1,
+                      target, current);
+}
+
