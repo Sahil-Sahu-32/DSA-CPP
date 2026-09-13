60cf1217a7d06923e7d89f0eb2fe6301d58d8e6f
Sahil-Sahu-32 <sahu02sahil@gmail.com>
2026-09-13 22:50:18 +0530

    Recuriton Q15 more

--- Recurtion/Q15.cpp
+++ Recurtion/Q15.cpp
@@ -43,4 +43,29 @@ bool search(vector<vector<char>>& board,
 
 int main() {
 
+    vector<vector<char>> board = {
+        {'A','B','C','E'},
+        {'S','F','C','S'},
+        {'A','D','E','E'}
+    };
+
+    string word = "ABCCED";
+
+    bool found = false;
+
+    for (int i = 0; i < board.size(); i++) {
+
+        for (int j = 0; j < board[0].size(); j++) {
+
+            if (search(board, word, i, j, 0)) {
+                found = true;
+            }
+        }
+    }
+
+    cout << (found ? "Found" : "Not Found");
+
+    cout << endl;
+
+    return 0;
 }
