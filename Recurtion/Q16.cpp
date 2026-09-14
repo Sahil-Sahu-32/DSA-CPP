485b0d2658bff17a23e2450f45fb0513464b8989
Sahil-Sahu-32 <sahu02sahil@gmail.com>
2026-09-14 23:12:58 +0530

    Recuriton Q16 more

--- Recurtion/Q16.cpp
+++ Recurtion/Q16.cpp
@@ -4,6 +4,47 @@ using namespace std;
 
 bool isPalindrome(string& s, int left, int right) {
 
+    while (left < right) {
+
+        if (s[left] != s[right]) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+void partition(string& s,
+               int index,
+               vector<string>& current) {
+
+    // Entire string processed
+    if (index == s.length()) {
+
+        for (string part : current) {
+            cout << part << " ";
+        }
+
+        cout << endl;
+
+        return;
+    }
+
+    for (int i = index; i < s.length(); i++) {
+
+        if (isPalindrome(s, index, i)) {
+
+            current.push_back(s.substr(index, i - index + 1));
+
+            partition(s, i + 1, current);
+
+            // Backtrack
+            current.pop_back();
+        }
+    }
 }
 
 int main() {
