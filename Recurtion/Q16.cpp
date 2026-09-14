dcdea9dd2e98646df8f1c81233d4f39faedb33e1
Sahil-Sahu-32 <sahu02sahil@gmail.com>
2026-09-14 23:12:43 +0530

    Recuriton Q16

---
+++ Recurtion/Q16.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isPalindrome(string& s, int left, int right) {
+
+}
+
+int main() {
+
+    string s = "aab";
+
+    vector<string> current;
+
+    partition(s, 0, current);
+
+    return 0;
+}
